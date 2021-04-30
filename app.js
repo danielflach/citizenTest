579f0d8b45eead3d4d4ffe24571eb1f1b3bacf3d
Daniel Flach <47660603+danielflach@users.noreply.github.com>
2021-04-30 13:32:52 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,416 @@
+let questionsList = [
+  {
+    question: "What is the supreme law of the land?",
+    answer: "🍔 the Constitution 🍔"
+  }, {
+    question: "What does the Constitution do?",
+    answer: "🍔 sets up the government 🍔 defines the government 🍔 protects basic rights of Americans 🍔"
+  }, {
+    question: "The idea of self - government is in the first three words of the Constitution. What are these words?",
+    answer: "🍔 We the People"
+  }, {
+    question: "What is an amendment?",
+    answer: "🍔 a change to the Constitution 🍔 an addition to the Constitution 🍔"
+  }, {
+    question: "What do we call the first ten amendments to the Constitution?",
+    answer: "🍔 the Bill of Rights 🍔"
+  }, {
+    question: "What do we call the first ten amendments to the Constitution?",
+    answer: "🍔 the Bill of Rights 🍔"
+  }, {
+    question: "What is one right or freedom from the First Amendment?",
+    answer: "🍔 speech 🍔 religion 🍔 assembly 🍔 press 🍔 petition the government 🍔"
+  }, {
+    question: "How many amendments does the Constitution have?",
+    answer: "🍔 twenty-seven (27) 🍔"
+  }, {
+    question: "What did the Declaration of Independence do?",
+    answer: "🍔 announced our independence from Great Britain 🍔 declared our independence from Great Britain 🍔 said that the United States is free from Great Britain 🍔"
+  }, {
+    question: "What are two rights in the Declaration of Independence?",
+    answer: "🍔 life 🍔 liberty 🍔 pursuit of happiness 🍔"
+  }, {
+    question: "What is freedom of religion?",
+    answer: "🍔 You can practice any religion, or not practice a religion. 🍔"
+  }, {
+    question: "What is the economic system in the United States?",
+    answer: "🍔 capitalist economy 🍔 market economy 🍔"
+  }, {
+    question: "What is the “rule of law”?",
+    answer: "🍔 Everyone must follow the law. 🍔 Leaders must obey the law 🍔 Government must obey the law 🍔 No one is above the law 🍔"
+  }, {
+    question: "Name one branch or part of the government.",
+    answer: "🍔 Congress 🍔 legislative 🍔 President 🍔 executive 🍔 the courts 🍔 judicial 🍔"
+  }, {
+    question: "What stops one branch of government from becoming too powerful?",
+    answer: "🍔 checks and balances 🍔 separation of powers 🍔"
+  }, {
+    question: "Who is in charge of the executive branch?",
+    answer: "🍔 the President 🍔"
+  }, {
+    question: "Who makes federal laws?",
+    answer: "🍔 Congress 🍔 Senate and House of Representatives 🍔 (U.S. or national) legislature 🍔"
+  }, {
+    question: "What are the two parts of the U.S.Congress?",
+    answer: "🍔 the Senate and House of Representatives 🍔"
+  }, {
+    question: "How many U.S. Senators are there?",
+    answer: "🍔 one hundred(100) 🍔"
+  }, {
+    question: "We elect a U.S. Senator for how many years?",
+    answer: "🍔 six (6) 🍔"
+  }, {
+    question: "Who is one of your state’s U.S. Senators now?",
+    answer: "🍔 Dianne Feinstein 🍔"
+  }, {
+    question: "The House of Representatives has how many voting members?",
+    answer: "🍔 four hundred thirty-five (435) 🍔"
+  }, {
+    question: "We elect a U.S. Representative for how many years?",
+    answer: "🍔 two(2) 🍔"
+  }, {
+    question: "Name your U.S. Representative.",
+    answer: "🍔 Scott Peters 🍔"
+  }, {
+    question: "Who does a U.S. Senator represent?",
+    answer: "🍔 all people of the state 🍔"
+  }, {
+    question: "Why do some states have more Representatives than other states?",
+    answer: "🍔 the state’s population 🍔 they have more people 🍔 some states have more people 🍔"
+  }, {
+    question: "We elect a President for how many years?",
+    answer: "🍔 four (4) 🍔"
+  }, {
+    question: "In what month do we vote for President?",
+    answer: "🍔 November 🍔"
+  }, {
+    question: "What is the name of the President of the United States now?",
+    answer: "🍔 Joe Biden 🍔"
+  }, {
+    question: "What is the name of the Vice President of the United States now?",
+    answer: "🍔 Kamala Harris 🍔"
+  }, {
+    question: "If the President can no longer serve, who becomes President?",
+    answer: "🍔 the Vice President 🍔"
+  }, {
+    question: "If both the President and the Vice President can no longer serve, who becomes President?",
+    answer: "🍔 the Speaker of the House 🍔"
+  }, {
+    question: "Who is the Commander in Chief of the military?",
+    answer: "🍔 the President 🍔"
+  }, {
+    question: "Who signs bills to become laws?",
+    answer: "🍔 the President 🍔"
+  }, {
+    question: "Who vetoes bills?",
+    answer: "🍔 the President 🍔"
+  }, {
+    question: "What does the President’s Cabinet do?",
+    answer: "🍔 advises the President 🍔"
+  }, {
+    question: "What are two Cabinet - level positions?",
+    answer: "🍔 Secretary of Agriculture 🍔 Secretary of Commerce 🍔 Secretary of Defense 🍔 Secretary of Education 🍔 Secretary of Energy 🍔 Secretary of Health and Human Services 🍔 Secretary of Homeland Security 🍔 Secretary of Housing and Urban Development 🍔 Secretary of the Interior 🍔 Secretary of Labor 🍔 Secretary of State 🍔 Secretary of Transportation 🍔 Secretary of the Treasury 🍔 Secretary of Veterans Affairs 🍔 Attorney General 🍔 Vice President 🍔"
+  }, {
+    question: "What does the judicial branch do?",
+    answer: "🍔 reviews laws 🍔 explains laws 🍔 resolves disputes (disagreements) 🍔 decides if a law goes against the Constitution 🍔"
+  }, {
+    question: "What is the highest court in the United States?",
+    answer: "🍔 the Supreme Court 🍔"
+  }, {
+    question: "How many justices are on the Supreme Court?",
+    answer: "🍔 nine (9) 🍔"
+  }, {
+    question: "Who is the Chief Justice of the United States now?",
+    answer: "🍔 John G. Roberts, Jr. 🍔"
+  }, {
+    question: "What is one power of the federal government?",
+    answer: "🍔 to print money 🍔 to declare war 🍔 to create an army 🍔 to make treaties 🍔"
+  }, {
+    question: "What is one power of the states?",
+    answer: "🍔 provide schooling and education 🍔 provide protection (police) 🍔 provide safety (fire departments) 🍔 give a driver’s license 🍔 approve zoning and land use 🍔"
+  }, {
+    question: "Who is the Governor of your state now?",
+    answer: "🍔 Gavin Newsom 🍔"
+  }, {
+    question: "What is the capital of your state?",
+    answer: "🍔 Sacramento 🍔"
+  }, {
+    question: "What are the two major political parties in the United States?",
+    answer: "🍔 Democratic and Republican 🍔"
+  }, {
+    question: "What is the political party of the President now?",
+    answer: "🍔 Democratic 🍔"
+  }, {
+    question: "What is the name of the Speaker of the House of Representatives now?",
+    answer: "🍔 Nancy Pelosi 🍔"
+  }, {
+    question: "There are four amendments to the Constitution about who can vote. Describe one of them.",
+    answer: "🍔 Citizens eighteen (18) and older (can vote). 🍔 You don’t have to pay (a poll tax) to vote. 🍔 Any citizen can vote. (Women and men can vote.) 🍔 A male citizen of any race(can vote). 🍔"
+  }, {
+    question: "What is one responsibility that is only for United States citizens?",
+    answer: "🍔 serve on a jury 🍔 vote in a federal election 🍔"
+  }, {
+    question: "Name one right only for United States citizens.",
+    answer: "🍔 vote in a federal election 🍔 run for federal office 🍔"
+  }, {
+    question: "What are two rights of everyone living in the United States?",
+    answer: "🍔 freedom of expression 🍔 freedom of speech 🍔 freedom of assembly 🍔 freedom to petition the government 🍔 freedom of religion 🍔 the right to bear arms 🍔"
+  }, {
+    question: "What do we show loyalty to when we say the Pledge of Allegiance?",
+    answer: "🍔 the United States 🍔 the flag 🍔"
+  }, {
+    question: "What is one promise you make when you become a United States citizen?",
+    answer: "🍔 give up loyalty to other countries 🍔 defend the Constitution and laws of the United States 🍔 obey the laws of the United States 🍔 serve in the U.S. military (if needed) 🍔"
+  }, {
+    question: "How old do citizens have to be to vote for President?",
+    answer: "🍔 eighteen(18) and older 🍔"
+  }, {
+    question: "What are two ways that Americans can participate in their democracy?",
+    answer: "🍔 vote 🍔 join a political party 🍔 help with a campaign 🍔 join a civic group 🍔 call Senators and Representatives 🍔 publicly support or oppose an issue or policy 🍔 run for office 🍔"
+  }, {
+    question: "When is the last day you can send in federal income tax forms ?",
+    answer: "🍔 April 15 🍔"
+  }, {
+    question: "When must all men register for the Selective Service?",
+    answer: "🍔 at age eighteen (18) 🍔 between eighteen(18) and twenty - six(26) 🍔"
+  }, {
+    question: "What is one reason colonists came to America?",
+    answer: "🍔 freedom 🍔 political liberty 🍔 religious freedom 🍔 economic opportunity 🍔 practice their religion 🍔 escape persecution 🍔"
+  }, {
+    question: "Who lived in America before the Europeans arrived?",
+    answer: "🍔 American Indians 🍔 Native Americans 🍔"
+  }, {
+    question: "What group of people was taken to America and sold as slaves?",
+    answer: "🍔 Africans 🍔 people from Africa 🍔"
+  }, {
+    question: "Why did the colonists fight the British?",
+    answer: "🍔 because of high taxes (taxation without representation) 🍔 because the British army stayed in their houses (boarding, quartering) 🍔 because they didn’t have self - government 🍔"
+  }, {
+    question: "Who wrote the Declaration of Independence?",
+    answer: "🍔 Thomas Jefferson 🍔"
+  }, {
+    question: "When was the Declaration of Independence adopted?",
+    answer: "🍔 July 4, 1776 🍔"
+  }, {
+    question: "There were 13 original states. Name three.",
+    answer: "🍔 New Hampshire 🍔 Massachusetts 🍔 Rhode Island 🍔 Connecticut 🍔 New York 🍔 New Jersey 🍔 Pennsylvania 🍔 Delaware 🍔 Maryland 🍔 Virginia 🍔 North Carolina 🍔 South Carolina 🍔 Georgia 🍔"
+  }, {
+    question: "What happened at the Constitutional Convention?",
+    answer: "🍔 The Constitution was written. 🍔 The Founding Fathers wrote the Constitution. 🍔"
+  }, {
+    question: "When was the Constitution written?",
+    answer: "🍔 1787 🍔"
+  }, {
+    question: "The Federalist Papers supported the passage of the U.S.Constitution. Name one of the writers.",
+    answer: "🍔 James Madison 🍔 Alexander Hamilton 🍔 John Jay 🍔 Publius 🍔"
+  }, {
+    question: "What is one thing Benjamin Franklin is famous for?",
+    answer: "🍔 U.S. diplomat 🍔 oldest member of the Constitutional Convention 🍔 first Postmaster General of the United States 🍔 writer of “Poor Richard’s Almanac” 🍔 started the first free libraries 🍔"
+  }, {
+    question: "Who is the “Father of Our Country”?",
+    answer: "🍔 George Washington 🍔"
+  }, {
+    question: "Who was the first President?",
+    answer: "🍔 George Washington 🍔" 
+  }, {
+    question: "What territory did the United States buy from France in 1803?",
+    answer: "🍔 the Louisiana Territory 🍔 Louisiana 🍔"
+  }, {
+    question: "Name one war fought by the United States in the 1800s.",
+    answer: "🍔 War of 1812 🍔 Mexican-American War 🍔 Civil War 🍔 Spanish-American War 🍔"
+  }, {
+    question: "Name the U.S.war between the North and the South.",
+    answer: "🍔 the Civil War 🍔 the War between the States 🍔"
+  }, {
+    question: "Name one problem that led to the Civil War.",
+    answer: "🍔 slavery 🍔 economic reasons 🍔 states’ rights 🍔"
+  }, {
+    question: "What was one important thing that Abraham Lincoln did?",
+    answer: "🍔 freed the slaves (Emancipation Proclamation) 🍔 saved (or preserved) the Union 🍔 led the United States during the Civil War 🍔"
+  }, {
+    question: "What did the Emancipation Proclamation do?",
+    answer: "🍔 freed the slaves 🍔 freed slaves in the Confederacy 🍔 freed slaves in the Confederate states 🍔 freed slaves in most Southern states 🍔"
+  }, {
+    question: "What did Susan B. Anthony do?",
+    answer: "🍔 fought for women’s rights 🍔 fought for civil rights 🍔"
+  }, {
+    question: "Name one war fought by the United States in the 1900s.",
+    answer: "🍔 World War I 🍔 World War II 🍔 Korean War 🍔 Vietnam War 🍔 (Persian) Gulf War 🍔"
+  }, {
+    question: "Who was President during World War I?",
+    answer: "🍔 Woodrow Wilson 🍔"
+  }, {
+    question: "Who was President during the Great Depression and World War II?",
+    answer: "🍔 Franklin Roosevelt 🍔"
+  }, {
+    question: "Who did the United States fight in World War II?",
+    answer: "🍔 Japan, Germany, and Italy 🍔"
+  }, {
+    question: "Before he was President, Eisenhower was a general. What war was he in?",
+    answer: "🍔 World War II 🍔"
+  }, {
+    question: "During the Cold War, what was the main concern of the United States?",
+    answer: "🍔 Communism 🍔"
+  }, {
+    question: "What movement tried to end racial discrimination?",
+    answer: "🍔 civil rights (movement) 🍔"
+  }, {
+    question: "What did Martin Luther King, Jr. do?",
+    answer: "🍔 fought for civil rights 🍔 worked for equality for all Americans 🍔"
+  }, {
+    question: "What major event happened on September 11, 2001, in the United States?",
+    answer: "🍔 Terrorists attacked the United States. 🍔"
+  }, {
+    question: "Name one American Indian tribe in the United States.",
+    answer: "🍔 Cherokee 🍔 Navajo 🍔 Sioux 🍔 Chippewa 🍔 Choctaw 🍔 Pueblo 🍔 Apache 🍔 Iroquois 🍔 Creek 🍔 Blackfeet 🍔 Seminole 🍔"
+  }, {
+    question: "Name one of the two longest rivers in the United States.",
+    answer: "🍔 Missouri (River) 🍔 Mississippi (River) 🍔"
+  }, {
+    question: "What ocean is on the West Coast of the United States?",
+    answer: "🍔 Pacific Ocean 🍔"
+  }, {
+    question: "What ocean is on the East Coast of the United States?",
+    answer: "🍔 Atlantic Ocean 🍔"
+  }, {
+    question: "Name one U.S. territory.",
+    answer: "🍔 Puerto Rico 🍔 U.S. Virgin Islands 🍔 American Samoa 🍔 Northern Mariana Islands 🍔 Guam 🍔"
+  }, {
+    question: "Name one state that borders Canada.",
+    answer: "🍔 Maine 🍔 New Hampshire 🍔 Vermont 🍔 New York 🍔 Pennsylvania 🍔 Ohio 🍔 Michigan 🍔 Minnesota 🍔 North Dakota 🍔 Montana 🍔 Idaho 🍔 Washington 🍔 Alaska 🍔"
+  }, {
+    question: "Name one state that borders Mexico.",
+    answer: "🍔 California 🍔 Arizona 🍔 New Mexico 🍔 Texas 🍔"
+  }, {
+    question: "What is the capital of the United States?",
+    answer: "🍔 Washington, D.C. 🍔"
+  }, {
+    question: "Where is the Statue of Liberty?",
+    answer: "🍔 New York (Harbor) 🍔 Liberty Island 🍔"
+  }, {
+    question: "Why does the flag have 13 stripes?",
+    answer: "🍔 because there were 13 original colonies 🍔 because the stripes represent the original colonies 🍔"
+  }, {
+    question: "Why does the flag have 50 stars?",
+    answer: "🍔 because there is one star for each state 🍔 because each star represents a state 🍔 because there are 50 states 🍔"
+  }, {
+    question: "What is the name of the national anthem?",
+    answer: "🍔 The Star-Spangled Banner 🍔"
+  }, {
+    question: "When do we celebrate Independence Day?",
+    answer: "🍔 July 4 🍔"
+  }, {
+    question: "Name two national US holidays.",
+    answer: "🍔 New Year’s Day 🍔 Martin Luther King, Jr. Day 🍔 Presidents’ Day 🍔 Memorial Day 🍔 Independence Day 🍔 Labor Day 🍔 Columbus Day 🍔 Veterans Day 🍔 Thanksgiving 🍔 Christmas 🍔"
+  }];
+
+let questionBox = document.getElementById('questionBox');
+let questionAnswer = questionBox.getElementsByTagName('p')[0];
+let answerBox = document.getElementById('answerBox');
+let answerAnswer = answerBox.getElementsByTagName('p')[0];
+let randomIndex = -1;
+
+function randomQuestion() {
+    randomIndex = Math.floor(Math.random() * questionsList.length);
+    let randomQuestion = questionsList[randomIndex];
+    questionAnswer.innerText = randomQuestion.question;
+    answerAnswer.innerText = '';
+  }
+
+function showAnswer() {
+  if (randomIndex != -1) {
+    let randomQuestion = questionsList[randomIndex];
+    answerAnswer.innerText = randomQuestion.answer;
+  }
+}
+
+function styleTrump() {
+  let element = document.getElementById("toggle");
+  element.classList.toggle("v1");
+}
+
+function toggleTrump() {
+  let element = document.getElementById("btn-trump");
+  element.classList.toggle("styleToggle");
+}
+
+const audioTrump = document.getElementById("audio-trump");
+function playTrump() {
+    if (audioTrump.paused) {
+        audioTrump.play();
+    }
+    else {
+        audioTrump.pause();
+    }
+};
+
+function Trump() {
+  styleTrump();
+  toggleTrump();
+  playTrump();
+}
+
+function styleExotic() {
+  let element = document.getElementById("toggle");
+  element.classList.toggle("v2");
+}
+
+function toggleExotic() {
+  let element = document.getElementById("btn-exotic");
+  element.classList.toggle("styleToggle");
+}
+
+const audioExotic = document.getElementById("audio-exotic");
+function playExotic() {
+    if (audioExotic.paused) {
+        audioExotic.play();
+    }
+    else {
+        audioExotic.pause();
+    }
+};
+
+function Exotic() {
+  styleExotic();
+  toggleExotic();
+  playExotic();
+}
+
+function styleHogan() {
+  let element = document.getElementById("toggle");
+  element.classList.toggle("v3");
+}
+
+function toggleHogan() {
+  let element = document.getElementById("btn-hogan");
+  element.classList.toggle("styleToggle");
+}
+
+const audioHogan = document.getElementById("audio-hogan");
+function playHogan() {
+    if (audioHogan.paused) {
+        audioHogan.play();
+    }
+    else {
+        audioHogan.pause();
+    }
+};
+
+function Hogan() {
+  styleHogan();
+  toggleHogan();
+  playHogan();
+}
+
+const audioEagle = document.getElementById("audio-eagle");
+function playEagle() {
+
+}
+
+
+function playEagle() {
+  const audio = document.getElementById("audio-eagle");
+  audio.currentTime = 0;
+  audio.play();
+}
+
